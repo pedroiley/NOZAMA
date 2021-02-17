459b7cef5553392ddafc4f820a61c1ccca9e2497
pedroiley <pedro.iglesias.leyva@gmail.com>
2021-02-17 19:57:36 +0200

    ADD userDao DATA ACCESS OBJECT

---
+++ src/main/java/dao/userDao.java
@@ -0,0 +1,80 @@
+package dao;
+
+import entity.user;
+import util.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import java.util.List;
+
+public class userDao{
+
+    public userDao() {
+    }
+
+    public void createUser(User user) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+
+    public User getUser(Long userId) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        try {
+            User user = session.find(User.class, userId);
+            session.close();
+            return user;
+        } catch (Exception ex) {
+            session.close();
+            System.out.println("Unable to find the user with id: " + userId);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<User> getUser() {
+        Session session = Hibernate.getSessionFactory().openSession();
+        return session.createQuery("from User", User.class).list();
+    }
+
+    public void updateUser(User savedUser) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(savedUser);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+
+    public void deleteUser(User savedUser) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(savedUser);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+}
