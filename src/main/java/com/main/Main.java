23681495a05a57f7f52a9fd0155ca7117deba492
Miquel Vidal <miquelvidalbieto@gmail.com>
2021-02-19 21:47:29 +0200

    Add Deletes for each entity

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,16 +1,47 @@
 package com.main;
 
+import dao.CartDao;
+import dao.ProductDao;
 import dao.UserDao;
+import entity.Cart;
+import entity.Product;
 import entity.User;
 import util.Role;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         User u = new User("Pedro","Iglesias","Fake@Email","Test@123", Role.Admin);
         UserDao UDao = new UserDao();
-
-
         UDao.createUser(u);
+
+
+//        DeleteUser();
+//        DeleteCart();
+//        DeleteCart();
+    }
+
+    private static void CreateUser(){
+
+    }
+
+    private static void DeleteUser() {
+        UserDao UDao = new UserDao();
+        List<User> UserList = UDao.getUser();
+        UDao.deleteUser(UserList.get(0));
+    }
+
+    private static void DeleteCart() {
+        CartDao CDao = new CartDao();
+        List<Cart> CartList = CDao.getCart();
+        CDao.deleteCart(CartList.get(0));
+    }
+
+    private static void DeleteProduct() {
+        ProductDao PDao = new ProductDao();
+        List<Product> ProductList = PDao.getProduct();
+        PDao.deleteProduct(ProductList.get(0));
     }
 }
